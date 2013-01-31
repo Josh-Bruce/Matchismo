55cd40f21e1f4919debddcf35e9d1455ce331657
Josh Bruce <josh@surftrack.co.uk>
2013-01-31 09:45:04 +0000

    Homework Assignment task 7, adding a background image to the playing card when they are not selected, completed.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -46,6 +46,14 @@
         cardButton.selected = card.isFaceUp;
         cardButton.enabled = !card.isUnplayable;
         cardButton.alpha = (card.isUnplayable ? 0.3 : 1.0);
+        
+        // Setting the background of the card
+        UIImage *cardBack = [UIImage imageNamed:@"playing_card.png"];
+        [cardButton setTitle:@"" forState:UIControlStateNormal];
+        [cardButton setImage:cardBack forState:UIControlStateNormal];
+        if (card .isFaceUp) {
+            [cardButton setImage:nil forState:UIControlStateNormal];
+        }
     }
     self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", self.game.score];
 }
